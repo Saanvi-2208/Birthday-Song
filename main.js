1eb2018ac8d5ee67c61233ffd434f43ebb0c47ee
Saanvi-2208 <144683656+Saanvi-2208@users.noreply.github.com>
2023-12-12 12:56:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+
+function new_image()
+{
+    fabric.Image.fromURL("BirthdayImage.jpg",function(Img)
+    {
+        blockObject=Img;
+        blockObject.scaleToWidth(700);
+        blockObject.scaleToHeight(510);
+        blockObject.set(
+         {
+            top:0,
+            left:0
+         }
+        );
+        canvas.add(blockObject);
+    });	
+	
+}
+
+function playSound(){
+	x.play();
+}
